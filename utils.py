f8d22a4594cd8c19260af3060c53bbdca9e018cf
Kril <moulignier@et.esiea.fr>
2024-01-30 13:30:45 +0100

    starting cylinder coding

--- utils.py
+++ utils.py
@@ -1,7 +1,18 @@
 import csv
 
+# Create a diagonal cylinder
+def cylinder_define(px, py, pz):
+    eq_cylinder = (px ** 2 + py ** 2 + pz ** 2) - \
+               ((
+                        vector[0] * (px - arriving_point[0])
+                        + vector[1] * (py - arriving_point[1])
+                        + vector[2] * (pz - arriving_point[2])) ** 2
+                / (vector[0] ** 2) + (vector[1] ** 2) + (vector[2] ** 2))
+
+    return eq_cylinder
+
 # Chemin vers le fichier CSV
-csv_file = '/home/cyril/Downloads/PlayerData_20240129102610.csv'
+csv_file = '../PlayerData_20240129102610.csv'
 
 # Listes pour stocker les données
 position_data = []
@@ -38,13 +49,36 @@ for i in range(0, len(isSick_data), sublist_length):
     if len(sublist) == sublist_length:
         isSick_data_sublist.append(sublist)
 
-compression_finished = False
+print(position_data_sublist)
+print(isSick_data_sublist)
 
+compressed_list = []
+
+# Pour chaque liste de "sublist_length" éléments on compresse selon les déplacements du joueur
 for sublist in position_data_sublist:
-    starting_point = sublist[0][-1]
-    arriving_point = sublist[0][0]
+    i = 0
+    compressed_sublist = []
+    compression_finished = False
 
+    # Point de départ et d'arrivée du cylindre, point de départ étant fixé au dernier point position du joueur
+    starting_point = sublist[0][-1]
+    arriving_point = sublist[0][i]
+    vector = arriving_point - starting_point
+
+    #
     while not compression_finished:
-        compression_finished = True
+        check_point = sublist[0][i + 1]
+        result = cylinder_define(check_point)
+
+        if result = :
+            continue
+        else:
+            i += 1
+            arriving_point = sublist[0][i]
+
+    # On ajoute le dernier point manuellement
+    compressed_sublist.append(sublist[0][-1])
+    compressed_list.append(compressed_sublist)
+
 
 
